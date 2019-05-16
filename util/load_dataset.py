243c2ec373646963a65cf0db2ed1263ae3ce509c
egpthrs <harsh.c.gupta@ericsson.com>
2019-05-16 14:33:31 +0100

    Creating a learning repository for nn

---
+++ util/load_dataset.py
@@ -0,0 +1,20 @@
+import numpy as np
+import h5py
+
+def load_dataset():
+    train = h5py.File("../../../../Datasets/kaggle/cat_binary_classification/train_catvnoncat.h5")
+    train_set_x_orig = np.array(train["train_set_x"][:])
+    train_set_y_orig = np.array(train["train_set_y"][:])
+    
+    
+    test = h5py.File(
+        "../../../../Datasets/kaggle/cat_binary_classification/test_catvnoncat.h5")
+    test_set_x_orig = np.array(test["test_set_x"][:])
+    test_set_y_orig = np.array(test["test_set_y"][:])
+
+    classes = np.array(test["list_classes"][:])
+
+    train_set_y_orig = train_set_y_orig.reshape((1,train_set_y_orig[0]))
+    test_set_y_orig = test_set_y_orig.reshape((1, test_set_y_orig[0]))
+
+    return train_set_x_orig, train_set_y_orig, test_set_x_orig, test_set_y_orig
